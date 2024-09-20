6a8f5adf6398a3be6b99a932bb536228a3eb2592
bagenigilbert <bagenigilbert@gmail.com>
2024-09-21 02:41:07 +0300

    added connector folder with eccomerce backed database models defined

--- django_backend_db/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-# accounts/admin.py
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-# Use the existing UserAdmin to manage your CustomUser
-admin.site.register(CustomUser, UserAdmin)
