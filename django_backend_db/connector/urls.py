3954263b603495bf451245b7a287ff7f0ce8bdc7
bagenigilbert <bagenigilbert@gmail.com>
2024-09-21 13:03:48 +0300

    corrected erros

--- django_backend_db/connector/urls.py
+++ django_backend_db/connector/urls.py
@@ -1,33 +1,33 @@
 from django.urls import path, include
-from rest_framework import routers 
-from . views import (ProductViewSet, UserViewSet, VendorViewSet, CategoryViewSet, OrderViewSet, OrderItemViewSet, PaymentViewSet,
-                        ReviewViewSet, BlogViewSet, CartViewSet,CartItemViewSet, WishlistViewSet, ShippingViewSet, AnalyticsViewSet,
-                        RefundViewSet, ConfigurationViewSet, FAQViewSet, SubscriptionViewSet, CouponViewSet, TaxViewSet,
-                        NotificationViewSet, ContactViewSet)
+from rest_framework import routers
+from .views import (ProductViewSet, UserViewSet, VendorViewSet, CategoryViewSet, OrderViewSet, OrderItemViewSet, PaymentViewSet,
+                    ReviewViewSet, BlogViewSet, CartViewSet, CartItemViewSet, WishlistViewSet, ShippingViewSet, AnalyticsViewSet,
+                    RefundViewSet, ConfigurationViewSet, FAQViewSet, SubscriptionViewSet, CouponViewSet, TaxViewSet,
+                    NotificationViewSet, ContactViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'products', ProductViewSet)
 router.register(r'vendor', VendorViewSet)
-router.register(r'category', CategoryViewSet)
-router.register(r'order',OrderViewSet)
-router.register(r'order-item', OrderItemViewSet)
-router.register(r'payment', PaymentViewSet)
-router.register(r'review', ReviewViewSet)
-router.register(r'blog',BlogViewSet)
-router.register(r'cart', CartViewSet)
-router.register(r'refund', RefundViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'carts', CartViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'blogs', BlogViewSet)
+router.register(r'coupons', CouponViewSet)
 router.register(r'cart-item', CartItemViewSet)
-router.register(r'wishlist', WishlistViewSet)
-router.register(r'shipping',ShippingViewSet)
+router.register(r'wishlists', WishlistViewSet)
+router.register(r'notifications', NotificationViewSet)
+router.register(r'refunds', RefundViewSet)
+router.register(r'configurations', ConfigurationViewSet)
+router.register(r'contacts', ContactViewSet)
+router.register(r'faqs', FAQViewSet)
+router.register(r'order-item', OrderItemViewSet)
+router.register(r'payments', PaymentViewSet)
+router.register(r'shippings', ShippingViewSet)
+router.register(r'taxes', TaxViewSet)
 router.register(r'analytics', AnalyticsViewSet)
-router.register(r'configuration', ConfigurationViewSet)
-router.register(r'faq', FAQViewSet)
-router.register(r'subscription', SubscriptionViewSet)
-router.register(r'coupon',CouponViewSet)
-router.register(r'taxview', TaxViewSet)
-router.register(r'notification', NotificationViewSet)
-router.register(r'contact', ContactViewSet)
+router.register(r'subscriptions', SubscriptionViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
